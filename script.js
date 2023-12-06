6168e7b1fa9991e3b9efef311886a4d47bb62b70
Brilliannissa Zulfa <113734241+brilliswift@users.noreply.github.com>
2023-12-06 10:15:16 +0700

    Update

---
+++ script.js
@@ -0,0 +1,26 @@
+ 
+ 
+alert("Selamat Datang di Halaman Detail Laporan");
+
+ 
+ /*
+ // Get the current date
+const currentDate = new Date();
+
+ // Get the current month in YYYY-MM format
+const currentMonth = currentDate.toISOString().slice(0, 7);
+
+ // Set the value of the date input to the current month
+document.getElementById("myDateInput").value = currentMonth; */
+
+const date = new Date();
+date.getDate();
+document.getElementById("date").value = date.getDate();
+
+
+// kehadiran guru
+
+document.getElementById("class").innerHTML = "Hello World";
+
+// admin page
+
